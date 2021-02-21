c2cd117dbbacaab1679b7af102a2ae69ba49a6aa
Patryk Omiotek <patryk.omiotek@gmail.com>
2021-02-21 12:16:32 +0100

    mock service worker

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
+
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
